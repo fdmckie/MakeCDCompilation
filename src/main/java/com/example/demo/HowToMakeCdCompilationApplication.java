35cfd39b8dd154a80c3f8022587f18339593a463
fdmckie <fdmckie@gmail.com>
2019-09-25 16:34:26 -0400

    Website: How-To... - Week 4 In-class assignment

---
+++ src/main/java/com/example/demo/HowToMakeCdCompilationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToMakeCdCompilationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToMakeCdCompilationApplication.class, args);
+    }
+
+}
